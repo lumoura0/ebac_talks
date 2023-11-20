ab52e040bd13d93111ac7828d7d36c92cb6dee9a
Luis Fernando <luisfernando.mourag@gmail.com>
2023-11-20 14:54:17 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2022, 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function() {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+  const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+  const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs) / 1000);
+
+  document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+  if(distanciaAteOEvento < 0 ) {
+    clearInterval(contaHoras);
+  document.getElementById('contador').innerHTML = "Evento expirado";
+  }
+}, 1000);
